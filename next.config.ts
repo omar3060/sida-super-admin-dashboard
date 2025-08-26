3786153fbd5c7108913c78de481f754b0b03a71c
Omar <omarmahmoudcs@gmail.com>
2025-08-26 20:03:54 +0300

    Fix deployment errors 1

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,21 @@
-    const nextConfig = {
-  // basePath: "/",      
+import path from "path";
+
+const nextConfig = {
+  // basePath: "/",
   output: "export",
   eslint: {
     ignoreDuringBuilds: true,
   },
+  experimental: {
+    esmExternals: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.join(__dirname, "src"),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
-
