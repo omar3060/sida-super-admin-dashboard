981065af502392182038fd6430425e74cd368bc9
Omar <omarmahmoudcs@gmail.com>
2025-08-27 23:15:22 +0300

    edit

--- src/core/common/footer/commonFooter.tsx
+++ src/core/common/footer/commonFooter.tsx
@@ -4,11 +4,11 @@ export default function CommonFooter(){
     return (
         <div>
         <div className="footer d-sm-flex align-items-center justify-content-between border-top bg-white p-3">
-            <p className="mb-0">2014 - 2025 © DreamsPOS. All Right Reserved</p>
+            <p className="mb-0">2014 - 2025 © Caducs. All Right Reserved</p>
             <p>
                 Designed &amp; Developed by{" "}
                 <Link href="#" className="text-primary">
-                    Dreams
+                    Caducs
                 </Link>
             </p>
         </div>
